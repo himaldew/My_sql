8e11321ade134761092e71f06a23c32dcc2f16d5
himaldew <shusamb@yahoo.com>
2020-10-29 00:17:12 +0000

    some queries

--- test.sql
+++ test.sql
@@ -26,3 +26,21 @@ delete from Genre where Name = 'Traditional';
 
 /* Try to select the record that was just deleted */
 select * from Genre where Name = 'Traditional';
+
+/*Create a Query that shows: The name of a track, the name of it's MediaType, and the name of it's genre. 
+You'll need to join 3 tables together with the appropriate join columns. Add a filter to only show tracks 
+with a MediaType of "Protected AAC audio file" and a Genre of "Soundtrack".*/
+
+select Track.Name as Track, MediaType.Name as MediaType, Genre.Name as Genre from Track
+    -> Inner Join MediaType on Track.MediaTypeId = MediaType.MediaTypeId
+    -> Inner Join Genre on Track.GenreId= Genre.GenreId
+    -> where MediaType.Name ="Protected AAC audio file" and Genre.Name="Soundtrack";
++---------------+--------------------------+------------+
+
+/*Filter to only show results for the 'Grunge' playlist*/
+SELECT Playlist.Name AS Playlist, Track.Name AS Track, Album.Title AS Album, Artist.Name AS Artist FROM Playlist
+INNER JOIN PlaylistTrack ON Playlist.PlaylistId = PlaylistTrack.PlaylistId
+JOIN Track ON PlaylistTrack.TrackId = Track.TrackId
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist on Album.ArtistId = Artist.ArtistId
+WHERE Playlist.Name = 'Grunge';
